2ed170d9cd889aae7b94dd93d36d232b00ae027c
Maximo <maximomendana8@gmail.com>
2026-09-12 14:35:37 -0300

    Modulizo funciones Detalles y editarTarea

--- Index.js
+++ Index.js
@@ -1,6 +1,5 @@
 const prompt = require('prompt-sync')();
-const { Tareas, crearTarea, resolverValor, buscarID} = require("./modelo/tareas.js");
-const { mostrarDetalles } = require("./vista/tareas.js");
+const { Tareas, crearTarea} = require("./modelo/tareas.js");
 
 let op;
 
@@ -94,41 +93,6 @@ function verTareas(Tareas) {
     }while (op !== 0);  
 }
 
-function Detalles(tareaEncontrada) {
-    mostrarDetalles(tareaEncontrada);
-    let aux2;
-    do{
-        console.log("Desea editar algo de la tarea?\n");
-        console.log("[1] si" + "[0] no\n");
-        aux2 = parseInt(prompt());
-    }while (aux2 !== 0 && aux2 !== 1);
-    if(aux2 === 1){
-        editarTarea(tareaEncontrada);
-    }
-}
-
-function editarTarea(tareaEncontrada){ 
-    console.log("Estas editando la tarea: " + tareaEncontrada.Titulo + "\n" +
-        "- Si desea mantener cualquier valor, simplemente deje en blanco. \n" +
-        "- Si quiere dejar en blanco un campo, escriba un espacio. \n");
-    let nuevoTitulo = prompt("Ingrese el nuevo título de la tarea: ");
-    tareaEncontrada.Titulo = resolverValor(nuevoTitulo, tareaEncontrada.Titulo);
-    let nuevaDescripcion = prompt("Ingrese la nueva descripción de la tarea: ");
-    tareaEncontrada.Descripcion = resolverValor(nuevaDescripcion, tareaEncontrada.Descripcion);
-    let nuevaDificultad;
-    do{
-        nuevaDificultad = prompt("Ingrese la nueva dificultad de la tarea (1-3): ");
-    }while (nuevaDificultad !== "" && nuevaDificultad !== " " && (nuevaDificultad < "1" || nuevaDificultad > "3"));
-    tareaEncontrada.Dificultad = parseInt(resolverValor(nuevaDificultad, tareaEncontrada.Dificultad));
-    let nuevoEstado;
-    do {
-      nuevoEstado = prompt("2. Estado ([P]/[E]/[C]): ");
-    } while (nuevoEstado !== "" && nuevoEstado !== " " && nuevoEstado !== "P" && nuevoEstado !== "E" && nuevoEstado !== "C");
-    tareaEncontrada.Estado = resolverValor(nuevoEstado, tareaEncontrada.Estado);
-    let nuevaFechaVencimiento = prompt("Ingrese la nueva fecha de vencimiento de la tarea (dd/mm/aaaa): ");
-    tareaEncontrada.Vencimiento = resolverValor(nuevaFechaVencimiento, tareaEncontrada.Vencimiento);
-    console.log("Tarea editada correctamente: \n");
-}
 
 function agregarTarea() {
     let titulo = prompt("Ingrese el título de la tarea: ");
