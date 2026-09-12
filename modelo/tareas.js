4a43a8429df2ea143fcd394ad518ca3100c53000
Maximo <maximomendana8@gmail.com>
2026-09-12 13:10:35 -0300

    Agrego función buscarID

--- modelo/tareas.js
+++ modelo/tareas.js
@@ -25,4 +25,26 @@ function crearTarea(titulo, descripcion, dificultad, estado, vencimiento){
     Tareas.push(nuevaTarea);
 }
 
-module.exports = {Tareas, crearTarea, resolverValor};
+function buscarID(Tareas, aux, idsMostrados) {
+    let esValido = false;
+    let tareaEncontrada;
+    for (let i = 0; i < idsMostrados.length; i++) {
+        if (idsMostrados[i] === aux) {
+            esValido = true;
+        }
+    }
+    if (!esValido) {
+        return undefined;
+    } else {
+        for (let i = 0; i < Tareas.length; i++) {
+            if (Tareas[i].ID === aux) {
+                tareaEncontrada = Tareas[i];
+                break;
+            }
+        }
+    }
+
+    return tareaEncontrada;
+}
+
+module.exports = {Tareas, crearTarea, resolverValor, buscarID};
